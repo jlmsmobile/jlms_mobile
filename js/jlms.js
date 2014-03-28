1031a616786a06d01bbc3a2d78482c0a065375a6
smco3000 <smco3000@yahoo.com>
2014-03-28 16:19:11 +0300

    e

--- js/jlms.js
+++ js/jlms.js
@@ -956,7 +956,7 @@ $(document).ready( function() {
 								var elId = $(this).attr('elid');
 							
 								if(elType == 3){
-									var file = $('#file'+hwId).val();
+									var file = $('#file-'+hwId).val();
 									alert(file);
 									//var file = jlms.consts.DIR_IMAGES+'/messages.png';
 									if( file.length ) {
